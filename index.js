36cf6b0501a51fc97e029ae357ba5c7f3192110a
risto koort <ristokoort@yahoo.com>
2023-09-21 12:15:05 +0300

    Add a swagger json file, add package.json files to program

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const port =8080
 const swaggerui = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.json");
 
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
 
 app.get("/barber", (req,res)=> {
     res.send([
         {id:1,name:"Maire"},
-        {id:1,name:"Kalle"}
+        {id:2,name:"Kalle"}
     ])    
 })
 
 app.listen(port, () => {
     console.log(`Api up at: http://localhost:${port}`)
 })
+
+
+
+
