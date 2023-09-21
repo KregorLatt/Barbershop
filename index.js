ed8dbbc220f19169c5d2bf20d04c905f96744787
risto koort <ristokoort@yahoo.com>
2023-09-21 10:49:26 +0300

    Fix bug

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-const app = require ("express")()
+const app = require ('express')()
 const port =8080
 const swaggerui = require("swagger-ui-express")
-const swaggerDocument = require("./docs/swagger.json")
+const swaggerDocument = require("./docs/swagger.json");
 
-app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.get("/barber", (req,res)=> {
+    res.send([
+        {id:1,name:"Maire"},
+        {id:1,name:"Kalle"}
+    ])    
+})
 
 app.listen(port, () => {
     console.log(`Api up at: http://localhost:${port}`)
