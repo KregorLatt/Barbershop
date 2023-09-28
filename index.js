f01d619f4d5e65254237bec4c580ba95dcbe8838
risto koort <ristokoort@yahoo.com>
2023-09-28 11:43:00 +0300

    .

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const app = require ('express')()
+const express = require ('express')()
+const app = express()
 const port =8080
 const swaggerui = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.json");
 const yamljs = require("yamljs")
 const swaggerDocument=yamljs.load("./docs/swagger.yaml");
+const barbers = require("./Barbers/data")
 
+app.use(express.json())
 app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
 
 const barbers = [
@@ -22,6 +25,29 @@ app.get("/barber", (req,res)=> {
         {id:2,name:"Kalle"}
     ])    
 })
+
+app.post("barbers",(req,res)=>{
+    if (!req.body.name || !req.body.Contact_details){
+        return res.status(400).send({error: "One or all required parameters are missing"})
+    }
+    const createdBarber = barbers.create({
+        name:req.body.name,
+        Contact_details:req.body.Contact_details
+    })
+        res.status(201)
+            .location(`${getBaseurl(req)}/games/${createdBarber.id}`)
+            .send(createdBarber)
+})
+
+function getBaseurl(request) {
+    return (request.connection && request.connection.encrypted ? "https" : "http")
+                 + "://" + request.headers.host
+}
+
+app.get("/barbers",(req,res)=>{
+    res.send(players.getAll())
+})
+
 
 app.get('/barbers/:id', (req,res) => {
     if (typeof barber[req.params.id -1] === 'undefined'){
