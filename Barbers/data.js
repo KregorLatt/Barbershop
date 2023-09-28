55b7bc61f372a831d2595a128b42beccf579f2d8
risto koort <ristokoort@yahoo.com>
2023-09-28 11:41:19 +0300

    Save changes before branches

---
+++ Barbers/data.js
@@ -0,0 +1,23 @@
+let data= [
+    {id:1,name:"Kaire",Contact_details:"Kaire@gmail.com"},
+    {id:2,name:"Maire",Contact_details:"Maire@gmail.com"},
+    {id:3,name:"Toomas",Contact_details:"Toomas@gmail.com"},
+    {id:4,name:"Lauri",Contact_details:"Lauri@gmail.com"}
+]
+
+
+
+exports.getAll = () => {
+    return data.map(g => { return { "id": g.id, "name": g.name, "Contact_details":g.Contact_details}})
+}
+
+exports.getById = (id) => {
+    return data.find((thing)=> thing.id == parseInt(id))
+}
+
+exports.create = (newBarber) =>{
+    const newId = Math.max ( data.map((thing)=>thing.id)) +1
+    newBarber.id=newId
+    data.push(newBarber)
+        return newBarber
+}
