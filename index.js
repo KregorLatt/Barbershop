83bde8176682dddc932eeeca6ad0903729020663
ristokoort <114060658+ristokoort@users.noreply.github.com>
2023-10-11 14:29:53 +0300

    Remove from index

--- index.js
+++ index.js
@@ -6,20 +6,9 @@ const swaggerDocument = require("./docs/swagger.json");
 const yamljs = require("yamljs")
 const swaggerDocument=yamljs.load("./docs/swagger.yaml");
 const barbers = require("./Barbers/data")
-
 app.use(express.json())
 app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
 
-
-app.get("/barbers", (req, res) => {
-    res.send(barbers.getAll())
-})
-
-app.get("/barbers/:id" , (req, res) => {
-    res.send(barbers.getById(req.params.id))
-})
-
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`);
