8b599834b1cfbb28a9c8ea72ec968ce2773a07e2
ristokoort <114060658+ristokoort@users.noreply.github.com>
2023-10-11 14:12:45 +0300

    Reinvent main

--- Barbers/data.js
+++ Barbers/data.js
@@ -15,15 +15,4 @@ exports.getById = (id) => {
     return data.find((thing)=> thing.id == parseInt(id))
 }
 
-exports.create = (newBarber) =>{
-    const newId = Math.max ( data.map((thing)=>thing.id)) +1
-    newBarber.id=newId
-    data.push(newBarber)
-        return newBarber
-}
-exports.delete = (id)=>{
-    var toBeDeleted = this.getById(id)
-    if(toBeDeleted=== undefined) return
-    data = data.filter((e) => toBeDeleted.id != e.id)
-    return toBeDeleted
-}
+
--- index.js
+++ index.js
@@ -10,59 +10,22 @@ const barbers = require("./Barbers/data")
 app.use(express.json())
 app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
 
-const barbers = [
-    {id:1,name:"Kaire",Contact_details:"Kaire@gmail.com"},
-    {id:2,name:"Maire",Contact_details:"Maire@gmail.com"},
-    {id:3,name:"Toomas",Contact_details:"Toomas@gmail.com"},
-    {id:4,name:"Lauri",Contact_details:"Lauri@gmail.com"}
-    
-]
 
-
-app.get("/barber", (req,res)=> {
-    res.send([
-        {id:1,name:"Maire"},
-        {id:2,name:"Kalle"}
-    ])    
+app.get("/barbers", (req, res) => {
+    res.send(barbers.getAll())
 })
 
-app.post("barbers",(req,res)=>{
-    if (!req.body.name || !req.body.Contact_details){
-        return res.status(400).send({error: "One or all required parameters are missing"})
-    }
-    const createdBarber = barbers.create({
-        name:req.body.name,
-        Contact_details:req.body.Contact_details
-    })
-        res.status(201)
-            .location(`${getBaseurl(req)}/games/${createdBarber.id}`)
-            .send(createdBarber)
+app.get("/barbers/:id" , (req, res) => {
+    res.send(barbers.getById(req.params.id))
 })
 
-function getBaseurl(request) {
-    return (request.connection && request.connection.encrypted ? "https" : "http")
-                 + "://" + request.headers.host
-}
-
-app.get("/barbers",(req,res)=>{
-    res.send(players.getAll())
-})
-
-
-app.get('/barbers/:id', (req,res) => {
-    if (typeof barber[req.params.id -1] === 'undefined'){
-        return res.status(404).send({error:"Game not found"})
-    }
-    res.send(games[req.params.id -1 ])
-})
-
-app.get('/barbers/:id', (req,res) => {
-    res.send(games[req.params.id - 1 ])
-})
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
-    console.log(`Api up at: http://localhost:${port}`)
+    console.log(`API up at: http://localhost:${port}`);
 })
+
+
 
 
 
