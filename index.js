9310c5d4b348cc314b7d67d813337e7ead878b10
KregorLatt <114172721+KregorLatt@users.noreply.github.com>
2023-10-11 23:41:13 +0300

    Add function to get all services

--- index.js
+++ index.js
@@ -5,17 +5,21 @@ const swaggerui = require("swagger-ui-express")
 const swaggerDocument = require("./docs/swagger.json");
 const yamljs = require("yamljs")
 const swaggerDocument=yamljs.load("./docs/swagger.yaml");
-const barbers = require("./Barbers/data")
+const services = require("./services/data")
 app.use(express.json())
 app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
+
+app.get("/services", (req, res) => {
+    res.send(services.getAll())
+})
+
+app.get("/services/:id" , (req, res) => {
+    res.send(services.getById(req.params.id))
+})
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`);
 })
-
-
-
-
-
-
