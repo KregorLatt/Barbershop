a498617785f4b22921f2efdf3caff2f165f63b56
ristokoort <114060658+ristokoort@users.noreply.github.com>
2023-10-11 15:49:44 +0300

    Function to get all barbers

--- index.js
+++ index.js
@@ -9,6 +9,23 @@ const barbers = require("./Barbers/data")
 app.use(express.json())
 app.use("/docs", swaggerui.serve, swaggerui.setup(swaggerDocument))
 
+const barbers = [
+    {id:1,name:"Kaire",Contact_details:"Kaire@gmail.com"},
+    {id:2,name:"Maire",Contact_details:"Maire@gmail.com"},
+    {id:3,name:"Toomas",Contact_details:"Toomas@gmail.com"},
+    {id:4,name:"Lauri",Contact_details:"Lauri@gmail.com"}
+   
+]
+
+app.get("/barbers", (req, res) => {
+    res.send (barbers)
+})
+
+app.get("/barbers/:id" , (req, res) => {
+    res.send(barbers[req.params.id])
+})
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`);
